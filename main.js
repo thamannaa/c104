5917333291e0ba9d7568256aafca54ccab95a2c2
thamannaa <77442887+thamannaa@users.noreply.github.com>
2021-07-22 16:31:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>'
+    });
+};
+
+console.log("ml5 version",ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/WYXhifu7n/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("model Loaded");
+}
